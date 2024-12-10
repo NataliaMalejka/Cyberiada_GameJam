40e3e89602851cd1d2f66e742890921c229c4a40
NataliaMalejka <natalia280803@gmail.com>
2024-12-10 09:47:22 +0100

    PlayerMoving2

--- GameJamCyberiada/Assets/Scripts/WSADmoving.cs
+++ GameJamCyberiada/Assets/Scripts/WSADmoving.cs
@@ -3,13 +3,22 @@ using UnityEngine;
 public class WSADmoving : MonoBehaviour
 {
     [SerializeField] private float velocity;
-    private void Update()
+    private void FixedUpdate()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
 
-        transform.Translate(horizontalInput * velocity * Time.deltaTime, 0, 0);
-        transform.localScale = new Vector3(4 * horizontalInput, 4, 1);
+        transform.Translate(horizontalInput * velocity * Time.deltaTime, verticalInput * velocity * Time.deltaTime, 0);
+
+        if(horizontalInput != 0)
+            transform.localScale = new Vector3(4 * horizontalInput, 4, 1);
+
+
+        //verticalInput = Input.GetAxis("Vertical");
+
+        //transform.Translate(velocity, verticalInput * upVelocity * Time.deltaTime, 0);
+
+        //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
 
     }
 }
